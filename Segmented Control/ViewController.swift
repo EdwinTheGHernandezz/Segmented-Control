54d1f006700cbf78e8cdb95d9385852d5ca396ee
Edwin Gonzalez <edwincanelogh8@icloud.com>
2022-02-25 15:34:14 -0700

    MVP

--- Segmented Control/ViewController.swift
+++ Segmented Control/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var segmentOut: UISegmentedControl!
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.yellow]
+        segmentOut.setTitleTextAttributes(titleTextAttributes, for: .normal)
+        let titleTextAttributes2 = [NSAttributedString.Key.foregroundColor: UIColor.green]
+        segmentOut.setTitleTextAttributes(titleTextAttributes2, for: .selected)
+    
+        
+        
     }
 
-
+    @IBAction func segmentAction(_ sender: Any)
+    {
+        
+        
+        
+        
+        switch segmentOut.selectedSegmentIndex
+        {
+        case 0:
+            helloLabel.text = "Hello!"
+        case 1:
+            helloLabel.text = "Hola!"
+        case 2:
+            helloLabel.text = "Bonjour!"
+        default:
+            helloLabel.text = "Hello!"
+        
+            
+        }
+    }
+    
+    
+    
 }
 
